69acb7adfe88e5d50c3281af5d05e7b422b102ce
jaegyung-jeon <jeon8684@gmail.com>
2025-02-27 20:52:24 -0500

    multiply

---
+++ for-loop/multiplication table.py
@@ -0,0 +1,5 @@
+numbers_you_want_to_input=int(input("type the number"))
+
+for i in range(1,11):
+    result = i * numbers_you_want_to_input
+    print(f"{numbers_you_want_to_input}*{i}"+f"={numbers_you_want_to_input*i}")
