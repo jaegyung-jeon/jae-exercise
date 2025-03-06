31c13cb7eab0212fdc54adb8a16d46b6fbd4ab00
jaegyung-jeon <jeon8684@gmail.com>
2025-03-05 19:38:50 -0500

    countdown function

---
+++ while/counting down.py
@@ -0,0 +1,9 @@
+#write a function countdown(n) that takes an integer n and prints numbers from n to 1 using a while loop.
+
+def jae():
+    n=int(input("number\n"))
+    while n>0:
+        n-=1
+        print(n)
+    print("blast off")
+jae()
