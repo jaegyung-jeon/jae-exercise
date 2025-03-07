3e3212352df963fb0bf2ec986f15378d7518e6cc
jaegyung-jeon <jeon8684@gmail.com>
2025-03-06 21:53:43 -0500

    still can't get it...(reversing given string)

--- for-loop/pattern printing.py
+++ for-loop/pattern printing.py
@@ -6,7 +6,7 @@
 # *****
 
 
-for i in range(0,5):
-    for j in range(0,i+1):
+for i in range(5,0,-1):
+    for j in range(1,i+1):
         print("*",end="")
     print()
--- for-loop/reverse a string.py
+++ for-loop/reverse a string.py
@@ -3,4 +3,4 @@
 letter=input("type your letters\n")
 
 for i in range(-1,-len(letter)-1,-1):
-    print(letter[i], end="")
+    print(letter[i],end="+")
